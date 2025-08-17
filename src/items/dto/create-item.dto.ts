f008f3bb7ebfd3468f0de4e652c6fef9f12f19a5
Basanta <basantaparajuli.11@gmail.com>
2025-08-17 12:05:10 +0545

    entities, create-item dto and validation

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,1 +1,10 @@
-export class CreateItemDto {}
+import { IsBoolean, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateItemDto {
+  @IsString({ message: 'Name must be string!' })
+  @MinLength(3)
+  @MaxLength(20)
+  name: string;
+  @IsBoolean({ message: 'Public must be set either true or false' })
+  public: boolean;
+}
